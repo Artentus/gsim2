904b4c57a15c7103112b4c87bbfcb10bfbb77f72
Mathis Rech <mathisrech@gmx.de>
2024-06-10 04:33:06 +0200

    Improve benchmarking

--- benches/sim.rs
+++ benches/sim.rs
@@ -24,6 +24,117 @@ fn generate_sim() -> Simulator {
         wires.push(wire);
     }
 
+    for _ in 0..1000000 {
+        let output = builder.add_wire(1).unwrap();
+        match comp_dist.sample(&mut rng) {
+            0 => {
+                let input_a = *wires.choose(&mut rng).unwrap();
+                let input_b = *wires.choose(&mut rng).unwrap();
+                let _id = builder
+                    .add_component(AndGatePorts {
+                        inputs: &[input_a, input_b],
+                        output,
+                    })
+                    .unwrap();
+            }
+            1 => {
+                let input_a = *wires.choose(&mut rng).unwrap();
+                let input_b = *wires.choose(&mut rng).unwrap();
+                let _id = builder
+                    .add_component(OrGatePorts {
+                        inputs: &[input_a, input_b],
+                        output,
+                    })
+                    .unwrap();
+            }
+            2 => {
+                let input_a = *wires.choose(&mut rng).unwrap();
+                let input_b = *wires.choose(&mut rng).unwrap();
+                let _id = builder
+                    .add_component(XorGatePorts {
+                        inputs: &[input_a, input_b],
+                        output,
+                    })
+                    .unwrap();
+            }
+            3 => {
+                let input_a = *wires.choose(&mut rng).unwrap();
+                let input_b = *wires.choose(&mut rng).unwrap();
+                let _id = builder
+                    .add_component(NandGatePorts {
+                        inputs: &[input_a, input_b],
+                        output,
+                    })
+                    .unwrap();
+            }
+            4 => {
+                let input_a = *wires.choose(&mut rng).unwrap();
+                let input_b = *wires.choose(&mut rng).unwrap();
+                let _id = builder
+                    .add_component(NorGatePorts {
+                        inputs: &[input_a, input_b],
+                        output,
+                    })
+                    .unwrap();
+            }
+            5 => {
+                let input_a = *wires.choose(&mut rng).unwrap();
+                let input_b = *wires.choose(&mut rng).unwrap();
+                let _id = builder
+                    .add_component(XnorGatePorts {
+                        inputs: &[input_a, input_b],
+                        output,
+                    })
+                    .unwrap();
+            }
+            6 => {
+                let input = *wires.choose(&mut rng).unwrap();
+                let _id = builder
+                    .add_component(NotGatePorts { input, output })
+                    .unwrap();
+            }
+            7 => {
+                let input = *wires.choose(&mut rng).unwrap();
+                let enable = *wires.choose(&mut rng).unwrap();
+                let _id = builder
+                    .add_component(BufferPorts {
+                        input,
+                        enable,
+                        output,
+                    })
+                    .unwrap();
+            }
+            _ => unreachable!(),
+        }
+        wires.push(output);
+    }
+
+    builder.build().unwrap()
+}
+
+fn generate_sim_sorted() -> Simulator {
+    use rand::distributions::Uniform;
+    use rand::prelude::*;
+
+    let mut rng = StdRng::seed_from_u64(0);
+    let drive_dist = Uniform::new(0, 3);
+    let comp_dist = Uniform::new(0, 8);
+
+    let mut builder = SimulatorBuilder::default();
+    let mut wires = Vec::new();
+
+    for _ in 0..100 {
+        let wire = builder.add_wire(1).unwrap();
+        let drive = match drive_dist.sample(&mut rng) {
+            0 => LogicState::HIGH_Z,
+            1 => LogicState::LOGIC_0,
+            2 => LogicState::LOGIC_1,
+            _ => unreachable!(),
+        };
+        builder.set_wire_drive(wire, &drive).unwrap();
+        wires.push(wire);
+    }
+
     let mut and_gates = Vec::new();
     let mut or_gates = Vec::new();
     let mut xor_gates = Vec::new();
@@ -150,52 +261,28 @@ fn generate_sim() -> Simulator {
             .unwrap();
     }
 
-    let sim = builder.build().unwrap();
-
-    //if first {
-    //    let stats = sim.stats();
-
-    //    println!();
-    //    println!();
-    //    println!("Wires: {} ({})", stats.wire_count, stats.wire_alloc_size);
-    //    println!("    Width alloc: {}", stats.wire_width_alloc_size);
-    //    println!("    Drive alloc: {}", stats.wire_drive_alloc_size);
-    //    println!("    State alloc: {}", stats.wire_state_alloc_size);
-    //    println!(
-    //        "Components: {} + {} ({} + {})",
-    //        stats.small_component_count,
-    //        stats.large_component_count,
-    //        stats.component_alloc_size,
-    //        stats.large_component_alloc_size
-    //    );
-    //    println!("    Width alloc: {}", stats.output_width_alloc_size);
-    //    println!("    State alloc: {}", stats.output_state_alloc_size);
-    //    println!(
-    //        "Total memory: {}",
-    //        stats.wire_alloc_size
-    //            + stats.wire_width_alloc_size
-    //            + stats.wire_drive_alloc_size
-    //            + stats.wire_state_alloc_size
-    //            + stats.component_alloc_size
-    //            + stats.large_component_alloc_size
-    //            + stats.output_width_alloc_size
-    //            + stats.output_state_alloc_size
-    //    );
-    //}
-
-    sim
+    builder.build().unwrap()
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut sim = generate_sim();
+    let mut sorted_sim = generate_sim_sorted();
 
-    c.bench_function("sim", |b| {
-        b.iter(|| {
-            sim.reset();
-            let result = sim.run(u64::MAX);
-            assert!(matches!(result, SimulationRunResult::Ok));
+    c.benchmark_group("random graph")
+        .bench_function("random insertion order", |b| {
+            b.iter(|| {
+                sim.reset();
+                let result = sim.run(u64::MAX);
+                assert!(matches!(result, SimulationRunResult::Ok));
+            })
         })
-    });
+        .bench_function("sorted insertion order", |b| {
+            b.iter(|| {
+                sorted_sim.reset();
+                let result = sorted_sim.run(u64::MAX);
+                assert!(matches!(result, SimulationRunResult::Ok));
+            })
+        });
 }
 
 criterion_group!(benches, criterion_benchmark);
